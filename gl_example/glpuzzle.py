a288e88739b24fcf499758d762e08cbc84ce0301
Tz <thenzen34@mail.ru>
2019-05-22 15:10:58 +0300

    opengl examples

---
+++ gl_example/glpuzzle.py
@@ -0,0 +1,19 @@
+# https://www.opengl.org/archives/resources/code/samples/glut_examples/examples/examples.html
+
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+WIDTH = 4
+HEIGHT = 5
+PIECES = 10
+OFFSETX = -2
+OFFSETY = -2.5
+OFFSETZ = -0.5
+
+# Config = [[0 for y in range(WIDTH)] for x in range(HEIGHT)]
+
+class puzzle():
+    backptr
+    solnptr
+    pass
